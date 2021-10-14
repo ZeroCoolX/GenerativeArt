fff684b5fd1d952df5074a3850ed97a19f929581
cnpatton <botanicality@gmail.com>
2021-10-13 20:36:34 -0500

    Adding src files for initial repo

---
+++ art_input/config.js
@@ -0,0 +1,85 @@
+// import file system
+const fileSys = require("fs");
+
+// Modify these as needed
+const WIDTH = 1000;
+const HEIGHT = 1000;
+const INPUT_EXT = "png"
+const OUTPUT_EXT = "png"
+
+// removes \\ for / since all systems are ok with / but only windows allows \\
+const universalDir = __dirname.replace(/\\/g, '/');
+
+// removes the file extension
+const cleanName = (_dirtyName) =>
+{
+    let search = `.${INPUT_EXT}`;
+    let replacer = new RegExp(search, 'g');
+    return _dirtyName.replace(replacer, '');
+};
+
+// get all files within the given dir and return an object
+const getImagesForLayer = (_path) =>
+{
+    return fileSys
+    .readdirSync(_path)
+    .filter((_item) => !/(^|\/)\.[^\/\.]/g.test(_item))
+    .map((_i, _index) =>
+    {
+        let imageChoice = 
+        {
+            imageId: _index + 1,
+            imageName: cleanName(_i),
+            imageFileName: _i
+        };
+        return imageChoice;
+    });
+};
+
+// array of layer objects
+//  each layer object consists of information about that layer, including all image choices for that layer
+const layers = [
+    {
+        layerId: 1,
+        layerName: "background",
+        layerLocation: `${universalDir}/background/`,
+        images: getImagesForLayer(`${universalDir}/background/`),
+        positionOnCanvas: {x:0, y:0},
+        size: {width:WIDTH, height:HEIGHT}
+    },
+    {
+        layerId: 2,
+        layerName: "catgirl",
+        layerLocation: `${universalDir}/catgirl/`,
+        images: getImagesForLayer(`${universalDir}/catgirl/`),
+        positionOnCanvas: {x:0, y:0},
+        size: {width:WIDTH, height:HEIGHT}
+    },
+    {
+        layerId: 3,
+        layerName: "heart",
+        layerLocation: `${universalDir}/heart/`,
+        images: getImagesForLayer(`${universalDir}/heart/`),
+        positionOnCanvas: {x:0, y:0},
+        size: {width:WIDTH, height:HEIGHT}
+    },
+    {
+        // TODO: remove layerId and layerName perhaps
+        layerId: 4,
+        layerName: "speech_bubble",
+        layerLocation: `${universalDir}/speech_bubble/`,
+        images: getImagesForLayer(`${universalDir}/speech_bubble/`),
+        positionOnCanvas: {x:0, y:0},
+        size: {width:WIDTH, height:HEIGHT}
+    },
+    // {
+    //     layerId: 5,
+    //     layerName: "cat_peeking",
+    //     layerLocation: `${universalDir}/cat_peeking/`,
+    //     images: getImagesForLayer(`${universalDir}/cat_peeking/`),
+    //     positionOnCanvas: {x:0, y:0},
+    //     size: {width:WIDTH, height:HEIGHT}
+    // }
+];
+
+module.exports = {layers, WIDTH, HEIGHT, OUTPUT_EXT};
